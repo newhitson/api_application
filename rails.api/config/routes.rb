a2eeb9ebc153a82df6a45a1ca36203279c156d9d
Nick <newhitson@gmail.com>
2018-10-06 15:52:17 -0700

    add user routes

--- rails.api/config/routes.rb
+++ rails.api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, default: {format: :json} do
+    devise_scope :v1 do
+      post 'sign_up', to: "registrations#create"
+    end
+  end
 end
