a0b8bf821299f9cc7710d6c575dd1c843f1bd04d
Nick <newhitson@gmail.com>
2018-10-06 16:09:08 -0700

    user can sign up though api call

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,6 +1,7 @@
-class api::V1::RegistrationsController < Devise::RegistrationsController
+class Api::V1::RegistrationsController < Devise::RegistrationsController
+
   def create
-    user = user.new(user_params)
+    user = User.new(user_params)
     if user.save
       render json: {
         message: "Signed Up Successfully",
@@ -19,8 +20,17 @@ class api::V1::RegistrationsController < Devise::RegistrationsController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def ensure_params_exit
+    return if params[:user].present?
+    render json: {
+      message: "missing params",
+      is_success: false,
+      data: {}
+    }, status: :bad_request
   end
 end
