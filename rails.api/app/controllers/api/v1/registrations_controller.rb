b3d1d62ccf584e1cfa6f2ed679fdf8c45def51dc
Nick <newhitson@gmail.com>
2018-10-06 19:52:53 -0700

    add log out

--- rails.api/app/controllers/api/v1/registrations_controller.rb
+++ rails.api/app/controllers/api/v1/registrations_controller.rb
@@ -3,19 +3,9 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new(user_params)
     if user.save
-      render json: {
-        message: "Signed Up Successfully",
-        is_success: true,
-        data: {
-          user: user
-        }
-      }, status: :ok
+      json_response "Signed Up Successfully", true, {user: user}, :ok
     else
-      render json: {
-        message: "something wrong",
-        is_success: false,
-        data: {}
-      }, status: :unprocessable_entity
+      json_response "Something wrong", false, {}, :unprocessable_entity
     end
   end
 
@@ -25,12 +15,8 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
-  def ensure_params_exit
+  def ensure_params_exist
     return if params[:user].present?
-    render json: {
-      message: "missing params",
-      is_success: false,
-      data: {}
-    }, status: :bad_request
+    json_response "missing Params", false, {}, :bad_request
   end
 end
